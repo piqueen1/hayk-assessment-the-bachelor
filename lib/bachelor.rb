65a8e666c3c6d2907f073a076bdef9fecb153501
Finn Thye <piqueen@gmail.com>
2019-11-21 16:28:07 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,51 @@
+require 'pry'
+
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner_hash = data[season].find do | person |
+    person["status"] == "Winner"
+  end 
+
+  winner_hash["name"].split(" ")[0]
 end
 
+# => {"season 19"=>
+#   [{"name"=>"Ashley Iaconetti",
+#     "age"=>"26",
+#     "hometown"=>"Great Falls, Virginia",
+#     "occupation"=>"Nanny/Freelance Journalist",
+#     "status"=>""},
+#    {"name"=>"Becca Tilley",
+#     "age"=>"26",
+#     "hometown"=>"Shreveport, Louisiana[b]",
+#     "occupation"=>"Chiropractic Assistant",
+#     "status"=>""},
+
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.keys.each do | season |
+    worker = data[season].find do | contestant |
+      contestant["occupation"] == occupation
+    end
+
+    if worker 
+      return worker["name"]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  residents = 0
+
+  data.keys.each do | season |
+    data[season].each do | contestant |
+      #binding.pry
+      if contestant["hometown"] == hometown do
+        residents += 1
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
